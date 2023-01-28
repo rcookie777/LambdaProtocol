7255703288347510d1fb03a0f238d80107c7d776
Riley cook <cookril2@msu.edu>
2023-01-28 17:45:58 -0500

    package.json fixed

--- gun-client/src/Routers.js
+++ gun-client/src/Routers.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./views/HomePage.js";
 import Chat from "./components/Chat/index.js";
 import AuthRoute from "./components/auth/AuthRoute";
+import Signup from "./components/auth/SignUp";
 
 
 
@@ -9,7 +10,7 @@ export default function Routers() {
     return(
     <Routes>
         <Route exact path="/" element={<Home />} />
-
+        <Route exact path="/signup" element={<Signup/>} />
         <Route element={<AuthRoute />} />
             <Route exact path="/chat" element={<Chat />} />
     </Routes>
