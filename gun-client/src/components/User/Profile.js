b7b217e16910493a5c19e655eacdd82484414394
Riley cook <cookril2@msu.edu>
2023-01-29 01:12:18 -0500

    add profile

--- gun-client/src/components/User/Profile.js
+++ gun-client/src/components/User/Profile.js
@@ -1,10 +1,210 @@
-import React from "react";
+/* eslint-disable default-case */
+/* eslint-disable no-unreachable */
+import React, {useEffect, useReducer} from "react";
+import { user } from "../../gun/user";
+
+
+const currentState = {
+    classes: []
+}
+
+const reducer = (state, dispatched) => {
+    switch (dispatched.action) {
+      case "add":
+        let { classes } = dispatched;
+        return {
+          classes: [classes, ...state.classes]
+        }
+        break;
+      case "clear":
+        return { classes: [] }
+        break;
+    }
+  }
 
 
 export default function Profile() {
+    const [showClassModal, setShowClassModal] = React.useState(false);
+    const [className, setClassName] = React.useState("");
+    const [state, dispatch] = useReducer(reducer, currentState)
+    const [university, setUniversity] = React.useState("");
+
+    function saveUniversity(){
+        console.log("Setting New University");
+        console.log('Uni:' ,university);
+        user.get("univirsity").set(university);
+    }
+
+
+
+    function saveClass(){
+        console.log("Setting New Class");
+        console.log('classname:' ,className);
+        user.get("classes").set(className);
+    }
+
+    const courses = user.get("classes");
+
+    function loadClasses(){
+        console.log("Loading Courses");
+        courses.map().on((data, key) => {
+            dispatch({
+                action: "add",
+                classes: {
+                    className: data
+                }
+            })
+        });
+    }
+
+    const newClassArray = () => {
+        const formattedMessages = state.classes.filter((value, index) => {
+          const _value = JSON.stringify(value)
+          return (
+            index ===
+            state.classes.findIndex(obj => {
+              return JSON.stringify(obj) === _value
+            })
+          )
+        })
+    
+        return formattedMessages
+      }
+
+ 
+
+
+      useEffect(() => {
+        dispatch({ action: "clear" });
+        loadClasses();
+        }, []);
+
+            
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="p-16 bg-gray-900">
+            <div className="p-8 bg-white shadow mt-24">
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                    <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
+                        <div>
+                            <p className="font-bold text-gray-700 text-xl">22</p>
+                            <p className="text-gray-400">Friends</p>
+                        </div>
+                        <div>
+                            <p className="font-bold text-gray-700 text-xl">10</p>
+                            <p className="text-gray-400">Courses</p>
+                        </div>
+                        <div>
+                            <p className="font-bold text-gray-700 text-xl">89</p>
+                            <p className="text-gray-400">Posts</p>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </div>
+
+                    <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+                        <button
+                            className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+                        >
+                            Connect
+                        </button>
+                        <button
+                            className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+                            type="button"
+                            onClick={() => setShowClassModal(true)}
+                        >
+                            Edit
+                        </button>
+                        {showClassModal ? (
+                            <>
+                                <div
+                                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                >
+                                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                        {/*content*/}
+                                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                            {/*header*/}
+                                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                                <h3 className="text-3xl font-semibold">
+                                                    Add a Class
+                                                </h3>
+                                                <button
+                                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                    onClick={() => setShowClassModal(false)}
+                                                >
+                                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                        ×
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            {/*body*/}
+                                            <div className="relative p-6 flex-auto">
+                                                <div>
+                                                    <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                                                    <input  onChange={(e) => {setClassName(e.target.value.toUpperCase())}} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="CSE320" required />
+                                                </div>
+                                            </div>
+
+                                            <div className="relative p-6 flex-auto">
+                                                <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                                </p>
+                                            </div>
+                                            {/*footer*/}
+                                            <div className="flex justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                                <button
+                                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                    type="button"
+                                                    onClick={() => setShowClassModal(false)}
+                                                >
+                                                    Close
+                                                </button>
+                                                <button
+                                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                    type="button"
+                                                    onClick={
+                                                        () => {setShowClassModal(false) ; saveClass()}
+                                                    }
+                                                >
+                                                    Save Changes
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                            </>
+                        ) : null}
+                    </div>
+                </div>
+
+                <div className="mt-20 text-center border-b pb-6">
+                    <h1 className="text-4xl font-medium text-gray-700">Riley Cook <span className="font-light text-gray-500">27</span></h1>
+                    <p className="font-light text-gray-600 mt-3">Michigan State University</p>
+                    {newClassArray().map((courses, index) => [
+ 
+                                <div className="flex flex-col justify-center">
+                                    <p className="text-gray-600 text-center font-light lg:px-16">{courses.className}</p>
+                                </div>                            
+                    ])}
+
+                </div>
+
+                <div className="mt-12 flex flex-col justify-center">
+                    <p className="text-gray-600 text-center font-light lg:px-16"></p>
+                    {/* <button
+                        className="text-indigo-500 py-2 px-4  font-medium mt-4"
+                    >
+                        Show more
+                    </button> */}
+                </div>
+
+            </div>
         </div>
+
     )
 }
--- gun-client/src/gun/user.js
+++ gun-client/src/gun/user.js
@@ -4,7 +4,6 @@ import { gun } from "./gun";
 export const user = gun.user().recall({ sessionStorage: true });
 
 
-
 export const fetchUserPub = () => {
     if (user.is) {
         let pub = user.is.pub
@@ -13,13 +12,18 @@ export const fetchUserPub = () => {
 }
 
 
-
 export const fetchUserAlias = () => {
     if (user.is) {
         let alias = user.is.alias
         return alias
     }
 }
+
+export const setUserClass = (userClass) => {
+    console.log(userClass)
+    user.get("classes").set(userClass);
+}
+
 
 
 
