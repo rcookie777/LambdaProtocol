953007fb7e2a968528fc784d14fa40a0a8aa56f8
Riley cook <cookril2@msu.edu>
2023-01-29 02:07:22 -0500

    added university

--- gun-client/src/components/User/Profile.js
+++ gun-client/src/components/User/Profile.js
@@ -1,51 +1,103 @@
 /* eslint-disable default-case */
 /* eslint-disable no-unreachable */
-import React, {useEffect, useReducer} from "react";
+import React, { useEffect, useReducer } from "react";
 import { user } from "../../gun/user";
 
 
 const currentState = {
-    classes: []
+    classes: [],
+    university: [],
+    name: [],
+    friends: [],
 }
 
 const reducer = (state, dispatched) => {
     switch (dispatched.action) {
-      case "add":
-        let { classes } = dispatched;
-        return {
-          classes: [classes, ...state.classes]
-        }
-        break;
-      case "clear":
-        return { classes: [] }
-        break;
+        case "add":
+            let { classes } = dispatched;
+            return {
+                classes: [classes, ...state.classes]
+            }
+            break;
+        case "clear":
+            return { classes: [] }
+            break;
     }
-  }
+}
+// const reducerFriends = (state, dispatched) => {
+//     switch (dispatched.action) {
+//         case "add":
+//             let { friends } = dispatched;
+//             return {
+//                 classes: [friends, ...state.friends]
+//             }
+//             break;
+//         case "clear":
+//             return { friends: [] }
+//             break;
+//     }
+// }
+// const reducerUni = (stateUni, dispatchUni) => {
+//     switch (dispatchUni.action) {
+//         case "add":
+//             let { university } = dispatchUni;
+//             return {
+//                 university: [university, ...stateUni.university]
+//             }
+//             break;
+//         case "clear":
+//             return { university: [] }
+//             break;
+//     }
+// }
 
 
 export default function Profile() {
     const [showClassModal, setShowClassModal] = React.useState(false);
+    const [showUniModal, setShowUniModal] = React.useState(false);
+
     const [className, setClassName] = React.useState("");
     const [state, dispatch] = useReducer(reducer, currentState)
-    const [university, setUniversity] = React.useState("");
 
-    function saveUniversity(){
+    // const [stateFriends, dispatchFriends] = useReducer(reducerFriends, currentState)
+
+    // const [stateUni, dispatchUni] = useReducer(reducerUni, currentState)
+    const [currentUni, setUniversity] = React.useState("");
+
+    function saveUniversity() {
         console.log("Setting New University");
-        console.log('Uni:' ,university);
-        user.get("univirsity").set(university);
+        console.log('Uni:', currentUni);
+        user.get("university").set(currentUni);
     }
 
-
-
-    function saveClass(){
+    function saveClass() {
         console.log("Setting New Class");
-        console.log('classname:' ,className);
+        console.log('classname:', className);
         user.get("classes").set(className);
     }
 
+    // const friends = user.get("friends");
     const courses = user.get("classes");
 
-    function loadClasses(){
+    const university = user.get("university");
+
+    university.map().on((data, key) => {
+        
+    });
+    
+
+    function loadUserData() {
+        const uni = new Set()
+        university.map().on((data, key) => {
+            uni.add(data)
+        });
+        return uni
+    }
+    const [first] = loadUserData();
+    console.log("First", first);
+
+
+    function loadClasses() {
         console.log("Loading Courses");
         courses.map().on((data, key) => {
             dispatch({
@@ -59,27 +111,42 @@ export default function Profile() {
 
     const newClassArray = () => {
         const formattedMessages = state.classes.filter((value, index) => {
-          const _value = JSON.stringify(value)
-          return (
-            index ===
-            state.classes.findIndex(obj => {
-              return JSON.stringify(obj) === _value
-            })
-          )
+            const _value = JSON.stringify(value)
+            return (
+                index ===
+                state.classes.findIndex(obj => {
+                    return JSON.stringify(obj) === _value
+                })
+            )
         })
-    
+
         return formattedMessages
-      }
+    }
 
- 
+    // const newUniArray = () => {
+    //     const formattedMessages = stateUni.university.filter((value, index) => {
+    //         const _value = JSON.stringify(value)
+    //         return (
+    //             index ===
+    //             stateUni.university.findIndex(obj => {
+    //                 return JSON.stringify(obj) === _value
+    //             })
+    //         )
+    //     })
+
+    //     return formattedMessages
+    // }
+
+    // console.log("New Classes:", newClassArray())
+    // console.log("New Uni:", newUniArray())
 
 
-      useEffect(() => {
+    useEffect(() => {
         dispatch({ action: "clear" });
         loadClasses();
-        }, []);
+        loadUserData();
+    }, []);
 
-            
 
     return (
         <div className="p-16 bg-gray-900">
@@ -146,7 +213,7 @@ export default function Profile() {
                                             <div className="relative p-6 flex-auto">
                                                 <div>
                                                     <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-                                                    <input  onChange={(e) => {setClassName(e.target.value.toUpperCase())}} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="CSE320" required />
+                                                    <input onChange={(e) => { setClassName(e.target.value.toUpperCase()) }} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="CSE320" required />
                                                 </div>
                                             </div>
 
@@ -167,7 +234,7 @@ export default function Profile() {
                                                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                                     type="button"
                                                     onClick={
-                                                        () => {setShowClassModal(false) ; saveClass()}
+                                                        () => { setShowClassModal(false); saveClass() }
                                                     }
                                                 >
                                                     Save Changes
@@ -183,13 +250,71 @@ export default function Profile() {
                 </div>
 
                 <div className="mt-20 text-center border-b pb-6">
-                    <h1 className="text-4xl font-medium text-gray-700">Riley Cook <span className="font-light text-gray-500">27</span></h1>
-                    <p className="font-light text-gray-600 mt-3">Michigan State University</p>
+                    <h1 className="text-4xl font-medium text-gray-700">Riley Cook </h1>
+                    <p className="font-light text-gray-600 mt-3"> {first} <div className="font-light text-gray-500" onClick={() => setShowUniModal(true)}>Edit</div> </p>
+                    {showUniModal ? (
+                        <>
+                            <div
+                                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                            >
+                                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                    {/*content*/}
+                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                        {/*header*/}
+                                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                            <h3 className="text-3xl font-semibold">
+                                                Edit
+                                            </h3>
+                                            <button
+                                                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                onClick={() => setShowUniModal(false)}
+                                            >
+                                                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                    ×
+                                                </span>
+                                            </button>
+                                        </div>
+                                        {/*body*/}
+                                        <div className="relative p-6 flex-auto">
+                                            <div>
+                                                <input onChange={(e) => { setUniversity(e.target.value) }} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Michigan State University" required />
+                                            </div>
+                                        </div>
+
+                                        <div className="relative p-6 flex-auto">
+                                            <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                            </p>
+                                        </div>
+                                        {/*footer*/}
+                                        <div className="flex justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                            <button
+                                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={() => setShowUniModal(false)}
+                                            >
+                                                Close
+                                            </button>
+                                            <button
+                                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={
+                                                    () => { setShowUniModal(false); saveUniversity() }
+                                                }
+                                            >
+                                                Save Changes
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                        </>
+                    ) : null}
                     {newClassArray().map((courses, index) => [
- 
-                                <div className="flex flex-col justify-center">
-                                    <p className="text-gray-600 text-center font-light lg:px-16">{courses.className}</p>
-                                </div>                            
+
+                        <div className="flex flex-col justify-center">
+                            <p className="text-gray-600 text-center font-light lg:px-16">{courses.className}</p>
+                        </div>
                     ])}
 
                 </div>
