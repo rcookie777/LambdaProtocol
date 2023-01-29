99b3653f804fece0c957f984496b72393676093d
Riley cook <cookril2@msu.edu>
2023-01-29 02:21:23 -0500

    added name to DB

--- gun-client/src/components/User/Profile.js
+++ gun-client/src/components/User/Profile.js
@@ -8,7 +8,6 @@ const currentState = {
     classes: [],
     university: [],
     name: [],
-    friends: [],
 }
 
 const reducer = (state, dispatched) => {
@@ -55,14 +54,16 @@ const reducer = (state, dispatched) => {
 export default function Profile() {
     const [showClassModal, setShowClassModal] = React.useState(false);
     const [showUniModal, setShowUniModal] = React.useState(false);
+    const [showNameModal, setShowNameModal] = React.useState(false);
 
     const [className, setClassName] = React.useState("");
     const [state, dispatch] = useReducer(reducer, currentState)
 
     // const [stateFriends, dispatchFriends] = useReducer(reducerFriends, currentState)
 
-    // const [stateUni, dispatchUni] = useReducer(reducerUni, currentState)
     const [currentUni, setUniversity] = React.useState("");
+
+    const [currentName, setName] = React.useState("");
 
     function saveUniversity() {
         console.log("Setting New University");
@@ -76,25 +77,27 @@ export default function Profile() {
         user.get("classes").set(className);
     }
 
-    // const friends = user.get("friends");
+    function saveName() {
+        console.log("Setting New Name");
+        console.log('Name:', currentName);
+        user.get("name").set(currentName);
+    }
+
     const courses = user.get("classes");
 
     const university = user.get("university");
 
-    university.map().on((data, key) => {
-        
-    });
-    
+    const name = user.get("name");
 
-    function loadUserData() {
+
+    function loadUniData() {
         const uni = new Set()
         university.map().on((data, key) => {
             uni.add(data)
         });
         return uni
     }
-    const [first] = loadUserData();
-    console.log("First", first);
+    const [first] = loadUniData();
 
 
     function loadClasses() {
@@ -108,6 +111,16 @@ export default function Profile() {
             })
         });
     }
+
+    function loadName(){
+        const names = new Set()
+        name.map().on((data, key) => {
+            names.add(data)
+        });
+        return names
+    }
+    const [,nam] = loadName();
+
 
     const newClassArray = () => {
         const formattedMessages = state.classes.filter((value, index) => {
@@ -144,7 +157,8 @@ export default function Profile() {
     useEffect(() => {
         dispatch({ action: "clear" });
         loadClasses();
-        loadUserData();
+        loadUniData();
+        loadName();
     }, []);
 
 
@@ -250,7 +264,66 @@ export default function Profile() {
                 </div>
 
                 <div className="mt-20 text-center border-b pb-6">
-                    <h1 className="text-4xl font-medium text-gray-700">Riley Cook </h1>
+                    <div className="text-4xl font-medium text-gray-700" onClick={() => setShowNameModal(true)}> {nam} </div>
+                    {showNameModal ? (
+                            <>
+                                <div
+                                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                >
+                                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                        {/*content*/}
+                                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                            {/*header*/}
+                                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                                <h3 className="text-3xl font-semibold">
+                                                    Change Name
+                                                </h3>
+                                                <button
+                                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                    onClick={() => setShowNameModal(false)}
+                                                >
+                                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                        ×
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            {/*body*/}
+                                            <div className="relative p-6 flex-auto">
+                                                <div>
+                                                    <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                                                    <input onChange={(e) => { setName(e.target.value) }} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Riley Cook" required />
+                                                </div>
+                                            </div>
+
+                                            <div className="relative p-6 flex-auto">
+                                                <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                                </p>
+                                            </div>
+                                            {/*footer*/}
+                                            <div className="flex justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                                <button
+                                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                    type="button"
+                                                    onClick={() => setShowNameModal(false)}
+                                                >
+                                                    Close
+                                                </button>
+                                                <button
+                                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                    type="button"
+                                                    onClick={
+                                                        () => { setShowNameModal(false); saveName() }
+                                                    }
+                                                >
+                                                    Save Changes
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                            </>
+                        ) : null}
                     <p className="font-light text-gray-600 mt-3"> {first} <div className="font-light text-gray-500" onClick={() => setShowUniModal(true)}>Edit</div> </p>
                     {showUniModal ? (
                         <>
